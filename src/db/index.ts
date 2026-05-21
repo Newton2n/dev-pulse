3cd75ee88a954e101144aa893baaa9e0888cd966
Newton <156936866+Newton2n@users.noreply.github.com>
2026-05-21 19:10:33 +0600

    feat(db) : add schema design to create core tables

---
+++ src/db/index.ts
@@ -0,0 +1,39 @@
+import { Pool } from "pg";
+import config from "../config/dotenv";
+
+const pool = new Pool({
+  connectionString: config.connectionString,
+});
+
+const dbInit = async () => {
+  try {
+    await pool.query(`
+    CREATE TABLE IF NOT EXISTS users(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+    role VARCHAR(20) DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
+    created_at TIMESTAMP DEFAULT NOW(),
+    updated_at TIMESTAMP DEFAULT NOW()
+    )`);
+
+    await pool.query(`
+        CREATE TABLE IF NOT EXISTS issues(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(150) NOT NULL,
+        description TEXT CHECK (LENGTH(description) >= 20),
+        type VARCHAR(20) CHECK(type IN ('bug','feature_request')),
+        status VARCHAR(20) DEFAULT 'open' CHECK(status IN ('open','in_progress','resolved')),
+        reporter_id INT NOT NULL REFERENCES users(id),
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+    )`);
+
+    console.log("DATA BASE CONNECTED SUCCESSFULLY");
+  } catch (error) {
+    console.log("error to connect data base", error);
+  }
+};
+
+export default dbInit;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
-
+import dbInit from "./db";
 const main = () => {
+  dbInit();
   app.listen(5000, () => {
     console.log(`Server running at http://localhost:${5000}`);
   });
